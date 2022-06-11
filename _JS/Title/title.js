beb5e58defe52b6a8447a068a45f34645449efa8
AryaaSk <aryaa_saravana@outlook.com>
2022-06-11 11:46:03 +0100

    Added title screen with animation, now just have to turn the game into a PWA

--- _JS/Title/title.js
+++ _JS/Title/title.js
@@ -1,1 +1,13 @@
 "use strict";
+const InitButtonListeners = () => {
+    document.getElementById("playButton").onclick = () => {
+        location.href = "/Src/Main/main.html";
+    };
+    document.getElementById("installButton").onclick = () => {
+        console.log("Instructions on how to install as app");
+    };
+};
+const MAIN_TITLE = () => {
+    InitButtonListeners();
+};
+MAIN_TITLE();
